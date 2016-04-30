d8f32814d617b2e1382fda3e04667764debfa386
carolinaisaza1 <carito9994@gmail.com>
2016-04-29 21:52:51 -0500

    Se procesa la actualización de token de usuarios en Firebase

--- www/js/services.js
+++ www/js/services.js
@@ -1,19 +1,29 @@
 angular.module('starter.services', [])
     .factory('Auth', function($firebaseAuth, $rootScope) {
         var usersRef = new Firebase("https//mybankatom.firebaseio.com/users");
-        var users = usersRef.push();
         return {
             iniciarSesionGoogle: function() {
                 usersRef.authWithOAuthPopup("google", function(error, authData) {
                     $rootScope.uid = authData.uid;
-                        $rootScope.email = authData.google.email;
-                        $rootScope.nombreUsuario = authData.google.displayName;
-                        $rootScope.token = authData.token;
-                        $rootScope.fotoPerfil = authData.google.profileImageURL; 
-                        usersRef.orderByChild("email").equalTo(!$rootScope.email).on("child_added", function(snapshot) {
-                            console.log("No existe");
-                           
-                        });
+                    $rootScope.email = authData.google.email;
+                    $rootScope.nombreUsuario = authData.google.displayName;
+                    $rootScope.token = authData.token;
+                    $rootScope.fotoPerfil = authData.google.profileImageURL;
+                    var user = {
+                        name: $rootScope.nombreUsuario,
+                        email: $rootScope.email,
+                        token : $rootScope.token
+                    };
+                    usersRef.child($rootScope.uid).once('value', function(snapshot) {
+                        // if data exists
+                        if (snapshot.exists()) {
+                            snapshot.ref().update(user);
+                        } else {
+                            var payload = {};
+                            payload[$rootScope.uid] = user;
+                            snapshot.ref().parent().update(payload);
+                        }
+                    });
 
                 }, {
                     remember: "sessionOnly",
