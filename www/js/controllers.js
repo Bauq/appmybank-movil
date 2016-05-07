6053d7651dfa69ea3913b9a0f241419b65c724c7
carolinaisaza1 <carito9994@gmail.com>
2016-05-07 17:16:28 -0500

    Muestra nombre, correo y foto desde localstorage

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -6,24 +6,23 @@ angular.module('starter.controllers', ['starter.services'])
     $scope.login = function() {
         $rootScope.iniciarSesionGoogle();
     };
-
-
-
 })
 
 .controller('ProductsController', function($scope, $rootScope, Auth, $window) {
     $scope.productos = [];
-
+    $scope.nombreUsuario= $window.localStorage.nombreUsuario;
+    $scope.email = $window.localStorage.email;
+    $scope.fotoPerfil = $window.localStorage.fotoPerfil;
     $scope.listarProductosCliente = function() {
         Auth.listarProductos($window.localStorage.email, $window.localStorage.token).then(function(data) {
+          $rootScope.show();
            var producto;
-           console.log(data.data);
             for (producto = 0; producto < data.data.length; producto++) {
-                console.log(data.data[producto])
                 $scope.productos.push(data.data[producto]);
             }
+          $rootScope.hide();
         }).catch(function(error) {
-            console.log(error);
+            $rootScope.showAlert('error',error.data.data);
         })
 
     }
--- www/js/services.js
+++ www/js/services.js
@@ -1,5 +1,5 @@
 angular.module('starter.services', [])
-    .factory('Auth', function($firebaseAuth, $rootScope, $state,$window,$http) {
+    .factory('Auth', function($firebaseAuth, $rootScope, $state, $window, $http, $ionicPopup, $ionicLoading) {
         var usersRef = new Firebase("https//mybankatom.firebaseio.com/users");
         var user;
         var conexionLegacyApi = "http://mybank-legacy-api.herokuapp.com/api/";
@@ -12,25 +12,45 @@ angular.module('starter.services', [])
             });
         };
 
+        $rootScope.show = function(text) {
+            $rootScope.loading = $ionicLoading.show({
+                template: '<p class="item-icon-left">' + text + '<ion-spinner class= "spinner-energized" icon="crescent"/></p>',
+            });
+        };
+
+        $rootScope.hide = function() {
+            $ionicLoading.hide();
+        };
+
+        $rootScope.showAlert = function(titulo, cuerpo) {
+            var alertPopup = $ionicPopup.alert({
+                title: titulo,
+                template: cuerpo
+            });
+            alertPopup.then(function(res) {
+                $state.go('login');
+            });
+        };
+
         $rootScope.iniciarSesionGoogle = function() {
             usersRef.authWithOAuthPopup("google", function(error, authData) {
                 if (!error) {
-                    $window.localStorage.uid = authData.uid;
+                    $rootScope.uid = authData.uid;
                     $window.localStorage.email = authData.google.email;
                     $window.localStorage.nombreUsuario = authData.google.displayName;
                     $window.localStorage.token = authData.token;
-                   $window.localStorage.fotoPerfil = authData.google.profileImageURL;
+                    $window.localStorage.fotoPerfil = authData.google.profileImageURL;
                     user = {
                         name: $window.localStorage.nombreUsuario,
                         email: $window.localStorage.email,
                         token: $window.localStorage.token
                     };
-                    usersRef.child($window.localStorage.uid).once('value', function(snapshot) {
+                    usersRef.child($rootScope.uid).once('value', function(snapshot) {
                         if (snapshot.exists()) {
                             snapshot.ref().update(user);
                         } else {
                             var payload = {};
-                            payload[$window.localStorage.uid] = user;
+                            payload[$rootScope.uid] = user;
                             snapshot.ref().parent().update(payload);
                         }
                     });
@@ -43,8 +63,8 @@ angular.module('starter.services', [])
         };
 
         return {
-            listarProductos : function(email,token){
-                //return $http.get(conexionLegacyApi + '/product/getByEmail?email='+ email +'&token='+token);
+            listarProductos: function(email, token) {
+                //return $http.get(conexionLegacyApi + '/product/getByEmail?email=' + email + '&token=' + token);
                 return $http.get('http://mybank-legacy-api.herokuapp.com/api/product/getByEmail?email=tinez7g@gmail.com&token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ2IjowLCJkIjp7InVpZCI6InVuaXF1ZUlkMSIsInNvbWUiOiJhcmJpdHJhcnkiLCJkYXRhIjoiaGVyZSJ9LCJpYXQiOjE0NjI2NTQxMDV9.8bFpNa_oa2m2z85ZRkg59McHWXqXogm6B6p0kAiaDAo')
             }
         }
